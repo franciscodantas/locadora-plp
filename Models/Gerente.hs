4f609256a3dec0a3a37491b553c240240404b526
Francisco Antonio Dantas de Sousa <96065182+franciscodantas@users.noreply.github.com>
2023-05-10 16:09:44 -0300

    Delete Models directory

--- Models/Gerente.hs
+++
@@ -1,7 +0,0 @@
-module Models.Gerente where
-    import GHC.Generics
-    
-    data Gerente = Gerente {
-        identificador :: String,
-        nome:: String
-    } deriving (Show, Generic)
--- Models/Produto.hs
+++
@@ -1,7 +0,0 @@
-module Models.Produto where
-    import GHC.Generics
-
-    data Produto = Produto {
-        id:: String,
-        idProduto:: String
-    } deriving (Show, Generic)

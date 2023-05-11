a724a025ba117d4b8410a36f6df25f5ee1aa9b13
Victor Alexandre Cavalcanti Macedo <victoralexandre2303@gmail.com>
2023-05-11 12:16:22 -0300

    Update Main.hs  add comments

--- app/Main.hs
+++ app/Main.hs
@@ -33,47 +33,47 @@ import qualified Data.Time.Format as TimeFormat
 
 main::IO()
 main = do
-        putStr "\n======== Locadora - Sistema ========\n"
-        putStr "Selecione uma opção:\n"
-        putStr "1 - Cliente\n"
-        putStr "2 - Funcionário\n"
-        putStr "3 - Gerencia\n"
-        putStr "4 - Sair\n"
-        putStr "----> "
-        op <- readLn:: IO Int
-        opcaoSelecionada op
+        putStr "\n======== Locadora - Sistema ========\n" --v
+        putStr "Selecione uma opção:\n" --v
+        putStr "1 - Cliente\n" --v
+        putStr "2 - Funcionário\n" --v
+        putStr "3 - Gerencia\n" --v
+        putStr "4 - Sair\n" --v
+        putStr "----> " --v
+        op <- readLn:: IO Int --v
+        opcaoSelecionada op --v
 
-opcaoSelecionada:: Int -> IO()
-opcaoSelecionada op
-        | op == 1 = cliente
-        | op == 2 = funcionario
-        | op == 3 = gerente
-        | op == 4 = putStr "Saindo...\n"
-        | otherwise = do
-            putStr "Entrada inválida...\n"
+opcaoSelecionada:: Int -> IO() --v
+opcaoSelecionada op --v
+        | op == 1 = cliente --v
+        | op == 2 = funcionario --v
+        | op == 3 = gerente --v
+        | op == 4 = putStr "Saindo...\n" --v
+        | otherwise = do --v
+            putStr "Entrada inválida...\n" --v
             main
 
 cliente :: IO ()
 cliente = do
-  putStr "\n======== Locadora - Sistema - Cliente ========\n"
-  putStr "1 - Listar filmes\n" 
-  putStr "2 - Escolher filme\n"
-  putStr "3 - Listar series\n"
-  putStr "4 - Escolher serie\n"
-  putStr "5 - Listar jogos\n"
-  putStr "6 - Escolher jogo\n"
-  putStr "7 - Produto por categoria\n"
-  putStr "8 - Adicionar Filme ao carrinho\n"
-  putStr "9 - Adicionar Jogo ao carrinho\n"
-  putStr "10 - Adicionar Série ao carrinho\n"
-  putStr "11 - Remover do carrinho\n"
-  putStr "12 - Ver carrinho\n"
-  putStr "13 - Recomendações\n" --
-  putStr "14 - menu principal\n"
-  putStr "----> "
-  op <- readLn :: IO Int
+  putStr "\n======== Locadora - Sistema - Cliente ========\n" --v
+  putStr "1 - Listar filmes\n" --v
+  putStr "2 - Escolher filme\n" --v
+  putStr "3 - Listar series\n" --v
+  putStr "4 - Escolher serie\n" --v
+  putStr "5 - Listar jogos\n" --v
+  putStr "6 - Escolher jogo\n" --v
+  putStr "7 - Produto por categoria\n" --v
+  putStr "8 - Adicionar Filme ao carrinho\n" --v
+  putStr "9 - Adicionar Jogo ao carrinho\n" --v
+  putStr "10 - Adicionar Série ao carrinho\n" --v
+  putStr "11 - Remover do carrinho\n" --v
+  putStr "12 - Ver carrinho\n" --v
+  putStr "13 - Recomendações\n" --v
+  putStr "14 - menu principal\n" --v
+  putStr "----> " --v
+  op <- readLn :: IO Int --v
   -- limparTela
-  menuCliente op
+  menuCliente op --v
 
 subMenuDiasSemanas :: IO String
 subMenuDiasSemanas = do
@@ -97,11 +97,11 @@ subMenuRecs = do
 
 menuCliente :: Int -> IO ()
 menuCliente op
-  | op == 1 = do
+  | op == 1 = do --v
     lF <- FuncC.listarFilmes
     putStrLn lF
     cliente
-  | op == 2 = do
+  | op == 2 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       putStrLn "Nome do Filme:"
@@ -125,11 +125,11 @@ menuCliente op
             else do
               putStrLn "Opção inválida!"
               cliente
-  | op == 3 = do
+  | op == 3 = do --v
     lS <- FuncC.listarSeries
     putStrLn lS
     cliente
-  | op == 4 = do
+  | op == 4 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       putStrLn "Nome da Série:"
@@ -153,11 +153,11 @@ menuCliente op
             else do
               putStrLn "Opção inválida!"
               cliente
-  | op == 5 = do
+  | op == 5 = do --v
     lJ <- FuncC.listarJogos
     putStrLn lJ
     cliente
-  | op == 6 = do
+  | op == 6 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       putStrLn "Nome do Jogo:"
@@ -181,7 +181,7 @@ menuCliente op
             else do
               putStrLn "Opção inválida!"
               cliente
-  | op == 7 = do
+  | op == 7 = do --v
       escolha <- subMenuCategoria
       if escolha == "1"
         then do
@@ -209,7 +209,7 @@ menuCliente op
                 else do
                   putStrLn "Opção inválida!"
                   cliente
-  | op == 8 = do
+  | op == 8 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       putStrLn "Nome do Produto:"
@@ -217,7 +217,7 @@ menuCliente op
       resultado <- FuncC.adicionarFilmeAoCarrinho idCliente nomeProduto
       putStrLn resultado
       cliente
-  | op == 9 = do
+  | op == 9 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       putStrLn "Nome do Produto:"
@@ -225,7 +225,7 @@ menuCliente op
       resultado <- FuncC.adicionarJogoAoCarrinho idCliente nomeProduto
       putStrLn resultado
       cliente
-  | op == 10 = do
+  | op == 10 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       putStrLn "Nome do Produto:"
@@ -233,7 +233,7 @@ menuCliente op
       resultado <- FuncC.adicionarSerieAoCarrinho idCliente nomeProduto
       putStrLn resultado
       cliente
-  | op == 11 = do
+  | op == 11 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       putStrLn "Nome do Produto:"
@@ -241,63 +241,63 @@ menuCliente op
       resultado <- FuncC.removerProduto idCliente nomeProduto
       putStrLn resultado
       cliente
-  | op == 12 = do
+  | op == 12 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       resultado <- FuncC.listarProdutos idCliente
       putStrLn resultado
       cliente
-  | op == 13 = do
+  | op == 13 = do --v
       putStrLn "Seu id: "
       idCliente <- getLine
       escolha <- subMenuRecs
       recs <- FuncC.recomendacoes escolha idCliente
       putStrLn recs
       cliente
-  | op == 14 = main
-  | otherwise = do
-      putStr "Entrada inválida...\n"
-      cliente
+  | op == 14 = main --v
+  | otherwise = do --v
+      putStr "Entrada inválida...\n" --v
+      cliente --v
 
-funcionario:: IO()
-funcionario  = do
-    putStr "\n======== Locadora - Sistema - Funcionário ========\n"
-    putStr "1 - Listar filmes disponiveis\n"
-    putStr "2 - Listar series disponiveis\n"
-    putStr "3 - Listar jogos disponiveis\n"
-    putStr "4 - Cadastrar cliente\n"
-    putStr "5 - Listar clientes\n"
-    putStr "6 - Encerrar cadastro de cliente\n"
-    putStr "7 - Exibir historico cliente\n"
-    putStr "8 - Cadastrar série\n"
-    putStr "9 - Excluir série\n"
-    putStr "10 - Cadastrar filme\n"
-    putStr "11 - Excluir filme\n"
-    putStr "12 - Cadastrar jogo\n"
-    putStr "13 - Exlcuir jogo\n"
-    putStr "14 - menu principal\n"
-    putStr "----> "
-    op <- readLn:: IO Int
-    menuFuncionario op
+funcionario:: IO() --v
+funcionario  = do --v
+    putStr "\n======== Locadora - Sistema - Funcionário ========\n" --v
+    putStr "1 - Listar filmes disponiveis\n" --v
+    putStr "2 - Listar series disponiveis\n" --v
+    putStr "3 - Listar jogos disponiveis\n" --v
+    putStr "4 - Cadastrar cliente\n" --v
+    putStr "5 - Listar clientes\n" --v
+    putStr "6 - Encerrar cadastro de cliente\n" --v
+    putStr "7 - Exibir historico cliente\n" --v
+    putStr "8 - Cadastrar série\n" --v
+    putStr "9 - Excluir série\n" --v
+    putStr "10 - Cadastrar filme\n" --v
+    putStr "11 - Excluir filme\n" --v
+    putStr "12 - Cadastrar jogo\n" --v
+    putStr "13 - Exlcuir jogo\n" --v
+    putStr "14 - menu principal\n" --v
+    putStr "----> " --v
+    op <- readLn:: IO Int --v
+    menuFuncionario op --v
 
 menuFuncionario:: Int -> IO()
 menuFuncionario op
-    | op == 1 = do
+    | op == 1 = do --v
       putStrLn "Filmes cadastrados:"
       lF <- FuncF.listarFilmes
       putStrLn lF
       funcionario
-    | op == 2 = do
+    | op == 2 = do --v
       putStrLn "Series cadastradas:"
       lS <- FuncF.listarSeries
       putStrLn lS
       funcionario
-    | op == 3 = do
+    | op == 3 = do --v
       putStrLn "Jogos cadastrados:"
       lJ <- FuncF.listarJogos
       putStrLn lJ
-      funcionario
-    | op == 4 = do
+      funcionario 
+    | op == 4 = do --v
       putStrLn "Nome do Cliente:"
       nome <- getLine
       putStrLn "Id do Cliente:"
@@ -309,12 +309,12 @@ menuFuncionario op
       resultadoCadastro <- FuncF.cadastrarCliente nome idCliente id senha
       putStrLn resultadoCadastro
       funcionario
-    | op == 5 = do
+    | op == 5 = do --v
       putStrLn "Clientes cadastrados:"
       resultado <- FuncF.listarClientes
       putStrLn resultado
       funcionario
-    | op == 6 = do
+    | op == 6 = do --v
       putStrLn "Id do cliente:"
       id <- getLine
       putStrLn "Id do Funcionario:"
@@ -324,13 +324,13 @@ menuFuncionario op
       resultado <- FuncF.excluirCliente id idFun senha
       putStrLn resultado
       funcionario
-    | op == 7 = do
+    | op == 7 = do --v
       putStrLn "Id do cliente:"
       id <- getLine
       resultado <- FuncF.exibirHistorico id
       putStrLn resultado
       funcionario
-    | op == 8 = do
+    | op == 8 = do --v
       putStrLn "Id da serie:"
       idSerie <- getLine
       putStrLn "Nome da serie:"
@@ -348,7 +348,7 @@ menuFuncionario op
       resultado <- FuncF.cadastrarSerie idFun senha idSerie nome descricao categoria preco
       putStrLn resultado
       funcionario
-    | op == 9 = do
+    | op == 9 = do --v
       putStrLn "Id da Série:"
       id <- getLine
       putStrLn "Id do Funcionario:"
@@ -358,7 +358,7 @@ menuFuncionario op
       resultado <- FuncF.excluirSerie id idFun senha
       putStrLn resultado
       funcionario
-    | op == 10 = do
+    | op == 10 = do --v
       putStrLn "Id do filme:"
       idSerie <- getLine
       putStrLn "Nome do filme:"
@@ -376,7 +376,7 @@ menuFuncionario op
       resultado <- FuncF.cadastrarFilme idFun senha idSerie nome descricao categoria preco
       putStrLn resultado
       funcionario
-    | op == 11 = do
+    | op == 11 = do --v
       putStrLn "Id da Filme:"
       id <- getLine
       putStrLn "Id do Funcionario:"
@@ -386,7 +386,7 @@ menuFuncionario op
       resultado <- FuncF.excluirFilme id idFun senha
       putStrLn resultado
       funcionario
-    | op == 12 = do
+    | op == 12 = do --v
       putStrLn "Id do jogo:"
       idSerie <- getLine
       putStrLn "Nome do jogo:"
@@ -404,7 +404,7 @@ menuFuncionario op
       resultado <- FuncF.cadastrarJogo idFun senha idSerie nome descricao categoria preco
       putStrLn resultado
       funcionario
-    | op == 13 = do
+    | op == 13 = do --v
       putStrLn "Id da Jogo:"
       id <- getLine
       putStrLn "Id do Funcionario:"
@@ -415,25 +415,25 @@ menuFuncionario op
       putStrLn resultado
       funcionario
     | op == 14 = main
-    | otherwise = do
+    | otherwise = do --v
         putStr "Entrada inválida...\n"
         funcionario
 
-gerente:: IO()
-gerente = do
-    putStr "\n======== Locadora - Sistema - Gerente ========\n"
-    putStr "1- Cadastrar funcionário\n"
-    putStr "2- Exibir funcionário\n"
-    putStr "3- Listar funcionários\n"
-    putStr "4- Estatisticas de vendas\n"
-    putStr "5- menu principal\n"
-    putStr "----> "
-    op <- readLn:: IO Int
-    menuGerente op
+gerente:: IO() --v
+gerente = do --v
+    putStr "\n======== Locadora - Sistema - Gerente ========\n" --v
+    putStr "1- Cadastrar funcionário\n" --v
+    putStr "2- Exibir funcionário\n" --v
+    putStr "3- Listar funcionários\n" --v
+    putStr "4- Estatisticas de vendas\n" --v
+    putStr "5- menu principal\n" --v
+    putStr "----> " --v
+    op <- readLn:: IO Int --v
+    menuGerente op --v
 
 menuGerente:: Int -> IO()
 menuGerente op
-    | op == 1 = do
+    | op == 1 = do --v
       putStrLn "Identidade do Funcionario:"
       id <- getLine
       putStrLn "Nome:"
@@ -443,17 +443,17 @@ menuGerente op
       resultado <- FuncG.cadastraFunc id nome senha
       putStrLn resultado
       gerente
-    | op == 2 = do
+    | op == 2 = do --v
       putStrLn "Identidade do Funcionario:"
       id <- getLine
       exibi <- FuncG.exibirFuncionario id
       putStrLn exibi
       gerente
-    | op == 3 = do 
+    | op == 3 = do --v
       lista <- FuncG.listarFun
       putStrLn lista
       gerente
-    | op == 4 = do
+    | op == 4 = do --v
         estatisticasString <- FuncG.estatisticas --
         totalClientesString <- FuncG.totalClientes -- 
         rendaF <- FuncG.rendaFilmes
@@ -473,7 +473,7 @@ menuGerente op
         putStrLn totalJ
 
         gerente
-    | op == 5 = main
-    | otherwise = do
-        putStr "Entrada inválida...\n"
-        gerente
+    | op == 5 = main --v
+    | otherwise = do --v
+        putStr "Entrada inválida...\n" --v
+        gerente --v

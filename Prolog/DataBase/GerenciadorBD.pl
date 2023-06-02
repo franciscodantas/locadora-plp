6a1cb43dd86d934654aa1551fc78b85b3dc6a5fe
João Juvino <joao.pedro.santos@ccc.ufcg.edu.br>
2023-06-01 21:41:48 -0300

    feat: adição da busca de filmes por nome

--- Prolog/DataBase/GerenciadorBD.pl
+++ Prolog/DataBase/GerenciadorBD.pl
@@ -23,9 +23,18 @@ seach_id([Head_Object|_], Id, Head_Object) :-
     Object_Id = Id, !.
 seach_id([_|Tail], Id, Object) :- seach_id(Tail, Id, Object).
 
+seach_nome([Head_Object|_], Nome, Head_Object) :- 
+    extract_info_produtos([Head_Object|_], _, Object_Nome, _, _, _, _),
+    Object_Nome = Nome, !.
+seach_nome([_|Tail], Nome, Object) :- seach_nome(Tail, Nome, Object).
+
 get_object_by_id(File, Id, Object) :- 
     load_json_file(File, Data),
     seach_id(Data, Id, Object).
+
+get_object_by_nome(File, Nome, Object) :- 
+    load_json_file(File, Data),
+    seach_nome(Data, Nome, Object).
 
 %%% REGRAS PARA FILMES %%%
 get_filmes(Data) :- load_json_file('Filme.json', Data).
@@ -33,6 +42,7 @@ add_filme(ID, Nome, Descricao, Categoria, Preco) :-
     Filme = json([id=ID, nome=Nome, descricao=Descricao, categoria=Categoria, preco=Preco, qtdAlugueis=0]),
     save_object('Filme.json', Filme).
 get_filme_by_id(Id, Filme) :- get_object_by_id('Filme.json', Id, Filme).
+get_filme_by_nome(Nome, Filme) :- get_object_by_nome('Filme.json', Nome, Filme).
 
 %%% REGRAS PARA SÉRIES %%%
 get_series(Data) :- load_json_file('Serie.json', Data).
@@ -47,3 +57,4 @@ add_jogo(ID, Nome, Descricao, Categoria, Preco) :-
     Jogo = json([id=ID, nome=Nome, descricao=Descricao, categoria=Categoria, preco=Preco, qtdAlugueis=0]),
     save_object('Jogo.json', Jogo).
 get_jogo_by_id(Id, Jogo) :- get_object_by_id('Jogo.json', Id, Jogo).
+

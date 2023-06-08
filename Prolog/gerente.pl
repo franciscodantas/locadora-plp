1fcd763560d2b69c029b7eedf7386893c44d4c40
João Juvino <joao.pedro.santos@ccc.ufcg.edu.br>
2023-06-08 17:24:24 -0300

    feat: adição de ume regra específica para obter os três jogos mais alugados(essa regra utiliza as regras gerais anteriormente commitadas)

--- Prolog/gerente.pl
+++ Prolog/gerente.pl
@@ -99,3 +99,8 @@ get_top_series_mais_alugadas(Series_Mais_Alugadas) :-
 get_top_series_menos_alugadas(Series_Menos_Alugadas) :- 
     get_series(Serie),
     get_n_destaques(Serie, 3, 'menos_alugado', [], Series_Menos_Alugadas).
+
+/* Essa regra é responsável por pegar os 3 jogos mais alugados do sistema */
+get_top_jogos_mais_alugados(Jogos_Mais_Alugados) :- 
+    get_jogos(Jogos),
+    get_n_destaques(Jogos, 3, 'mais_alugado', [], Jogos_Mais_Alugados).
